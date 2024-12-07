5bb7b76ed35118f4152363cd02108048f3acd900
Abaiev-M <abaievmisha@gmail.com>
2024-12-07 19:43:30 +0200

    projects added

---
+++ SwiftUI/HomeWork6SU/HomeWork6SU/HomeWork6SUApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeWork6SUApp.swift
+//  HomeWork6SU
+//
+//  Created by Misha Abaiev on 07.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct HomeWork6SUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
